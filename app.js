5d3f89ddd291c08918c1716be053a4b8e837c932
Mauro0800 <mauroamado000@gmail.com>
2023-01-13 02:17:40 -0300

    Agrega Formulario login.html y register.html

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ const path = require("path");
 const port = 3000;
 
 app.get('/',(req,res)=>res.sendFile(path.resolve(__dirname,'views','home.html')))
+app.get('/register',(req,res)=>res.sendFile(path.resolve(__dirname,'views','register.html')))
+app.get('/login',(req,res)=>res.sendFile(path.resolve(__dirname,'views','login.html')))
 app.use(express.static('public'))
 app.listen(port,()=> console.log(`servidor levantado en el puerto https//localhost:${ port } `))
